b85030538dd7eddd8517d760e5862a0abfb02c12
martinay <4411119+Martinay@users.noreply.github.com>
2018-07-13 10:13:15 +0200

    init

---
+++ ClusterWithClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Cluster.Tools.Client;
+
+namespace ClusterWithClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = @"
+akka {
+actor {
+        provider = ""Akka.Cluster.ClusterActorRefProvider, Akka.Cluster""
+    }
+remote {
+        helios.tcp {
+            hostname = ""127.0.0.1"" 
+            public-hostname = ""127.0.0.1""
+            port = 50000
+        }
+    }
+cluster {
+        seed-nodes = [""akka.tcp://ActorSystem@127.0.0.1:50000""]
+        roles = [def]
+    }
+}";
+
+            var actorSystem = ActorSystem.Create("ActorSystem", config);
+            ClusterClientReceptionist.Get(actorSystem);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClusterWithoutClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Cluster.Tools.Client;
+
+namespace ClusterWithoutClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = @"
+akka {
+actor {
+        provider = ""Akka.Cluster.ClusterActorRefProvider, Akka.Cluster""
+    }
+remote {
+        helios.tcp {
+            hostname = ""127.0.0.1""
+            public-hostname = ""127.0.0.1""
+            port = 60000
+        }
+    }
+cluster {
+        seed-nodes = [""akka.tcp://ActorSystem@127.0.0.1:50000""]
+        roles = [abc]
+    }
+}";
+
+            var actorSystem = ActorSystem.Create("ActorSystem", config);
+            
+            //ClusterClientReceptionist.Get(actorSystem);
+            
+            Console.ReadKey();
+        }
+    }
+}
